1addb65957d6273b14d8833f7bf028c001467409
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2025-11-23 16:59:12 -0800

    refactor: simplify getTestNameFromId function (SonarCloud)

--- client/src/features/testing/TestRunner.ts
+++ client/src/features/testing/TestRunner.ts
@@ -10,16 +10,8 @@ export class GroovyTestRunner {
         const suiteMarker = '?suite=';
         const testMarker = '?test=';
 
-        let marker = id.includes(testMarker) ? testMarker : suiteMarker;
-        const name = id.substring(id.indexOf(marker) + marker.length);
-        
-        // For test methods, the parent suite name is part of the ID.
-        if (marker === testMarker) {
-            return name;
-        }
-        
-        // For suites, just return the suite name.
-        return name;
+        const marker = id.includes(testMarker) ? testMarker : suiteMarker;
+        return id.substring(id.indexOf(marker) + marker.length);
     }
 
     public async run(workspaceFolder: vscode.WorkspaceFolder, tests: vscode.TestItem[]): Promise<void> {
